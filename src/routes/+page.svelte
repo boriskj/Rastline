d5b938c3a3d78e6e7703b03ec3861b9db683620c
boriskj <boris_krj@protonmail.com>
2024-10-16 19:39:25 +0200

    rastline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,59 @@
-<div class="grid justify-items-center gap-5">
-	<h1 class="bg-yellow-200 font-black text-2xl p-5 border-red-300 border-2 rounded-md text-center">
-		Hello World, Hello Grace!
-	</h1>
-	<img class="rounded-md w-96" src="art.webp" />
+<h1 class="text-3xl text-center">PREDSTAVITEV RASTLINE</h1>
+
+<p>
+	Rastlíne (znanstveno ime Plantae) so eno izmed kraljestev živih bitij, v katerega uvrščamo
+	približno 300.000 danes znanih vrst. Skupina vključuje splošno razširjene in poznane življenjske
+	oblike, kot so drevesa, cvetnice, trave, praprotnice in mahovi, izključuje pa rdeče in rjave alge,
+	živali, glive, arheje ter bakterije. Za rastline je značilno, da razen izjem pridobivajo energijo
+	za rast in delovanje organizma iz sončne svetlobe s pomočjo procesa fotosinteze, ki poteka v
+	kloroplastih, poleg tega imajo njihove celice celične stene, grajene iz celuloze.
+</p>
+<div class="grid grid-cols-3 gap-4 m-4 text-center">
+	<div class="bg-cyan-100 border-cyan-950 border-4 rounded-2xl">
+		<h1 class="font-bold">BANANOVEC</h1>
+		<img
+			class="rounded-lg"
+			src="//upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Luxor%2C_Banana_Island%2C_Banana_Tree%2C_Egypt%2C_Oct_2004.jpg/250px-Luxor%2C_Banana_Island%2C_Banana_Tree%2C_Egypt%2C_Oct_2004.jpg"
+		/>
+		<p>
+			Bananovec (znanstveno ime Musa) je rod večletnih rastlin tropskega izvora. Steblo ali bolj
+			pravilno imenovno »pseudosteblo« kjer listi tvorijo rozeto ki se samodejno podpira. V
+			vegetacijskem obdobju raste bananovec skupaj z obilico stranskih (hčerinskih) poganjkov.
+			Stranski poganjki se odstranjujejo zaradi koncentriranja hranil v starševski rastlini. Pri
+			vzgajanju se načrtno odbira ustrezni stranski poganjek za naslednjo rastno sezono po obliki
+			listov in vigorju. Po videzu bananovci spominjajo na palmo. Obstaja več sto različnih vrst
+			bananovcev. Njihov plod je banana.
+		</p>
+	</div>
+	<div class="bg-cyan-100 border-cyan-950 border-4 rounded-2xl">
+		<h1 class="font-bold">BAMBUS</h1>
+		<img
+			class="rounded-lg"
+			src="//upload.wikimedia.org/wikipedia/commons/thumb/2/2b/BambooKyoto.jpg/240px-BambooKyoto.jpg"
+		/>
+		<p>
+			Támbus (znanstveno ime Bambuseae) je trajnica iz družine trav z olesenelimi stebli. Obstaja
+			ogromno različnih vrst bambusa, ki jih najdemo praktično na vseh celinah. Bambus spada v
+			veliko družino Gramineae, ki vključuje tudi trave, žita, oves in koruzo. Bambus je trava in z
+			nekaj izjemami je tropska vrsta.
+		</p>
+	</div>
+	<div class="bg-cyan-100 border-cyan-950 border-4 rounded-2xl px-4">
+		<h1 class="font-bold">SONČNICA</h1>
+		<img
+			class="rounded-lg"
+			src="//upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/220px-Sunflower_sky_backdrop.jpg"
+		/>
+		<p>
+			Sončnica (znanstveno ime Helianthus (lat., izg. heliántus)) [3] je rod, ki obsega približno 70
+			vrst enoletnih in trajnic cvetočih rastlin iz družine nebinovk (Asteraceae).[4][5] Razen treh
+			južnoameriških vrst so ostale iz rodu Helianthus izvorno doma v Severni in Srednji Ameriki.
+			Ljudski imeni "sončnica" in "navadna sončnica" se običajno nanašata na priljubljeno enoletno
+			vrsto Helianthus annuus, katere okrogle cvetne glavice v kombinaciji z liguli izgledajo kot
+			sonce. [6] To in druge vrste, predvsem topinambur (H. tuberosus), gojijo v zmernih regijah in
+			nekaterih tropskih regijah kot prehranske rastline za ljudi, govedo in perutnino ter kot
+			okrasne rastline.[7] Navadna sončnica običajno raste poleti in v zgodnji jeseni, osrednja
+			sezona rasti pa je sredi poletja.[8]
+		</p>
+	</div>
 </div>
